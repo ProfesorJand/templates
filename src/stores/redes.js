9d961fe3eb5d309f875d61562f3f7b7290c94c1c
profesorjand <profesorjand@gmail.com>
2026-04-03 15:35:33 -0300

    Adding Carousel of Cards

---
+++ src/stores/redes.js
@@ -0,0 +1,12 @@
+export const redes = {
+  instagram: "https://www.instagram.com/tu_usuario",
+  facebook: "https://www.facebook.com/tu_usuario",
+  x: "https://www.x.com/tu_usuario",
+  linkedin: "https://www.linkedin.com/tu_usuario",
+  youtube: "https://www.youtube.com/tu_usuario",
+  tiktok: "https://www.tiktok.com/tu_usuario",
+  whatsapp: "https://wa.me/tu_numero",
+  telegram: "https://t.me/tu_usuario",
+  email: "[EMAIL_ADDRESS]",
+  
+}
